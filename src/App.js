cbed7bb99875fda567a024654ba506cb01891b07
Sabina <sabina.sadowska@gmail.com>
2019-12-30 17:16:35 +0100

    tabs corrected, scss file created for header component, variables name updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
-import Header from './Components/Header'
+import Header from './Components/Header/Header'
 
 function App() {
   return (
-  <Header/>
+    <Header/>
   );
 }
 
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import axios from 'axios';
+import './header.scss'
+
+class Header extends React.Component {
+    state = {
+        movieTitle: [],
+        movieDescription: [],
+        photo: []
+    }
+
+    componentDidMount() {
+        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=68d7264858afc32ecd7aaaf941bb14b7&language=en-US')
+            .then(res => {
+                let randomNumber =   Math.floor(Math.random() * (19 - 0)) + 0; 
+                const data = res.data;
+                this.setState({
+                    movieTitle: data.results[randomNumber].title,
+                    movieDescription: data.results[randomNumber].overview,
+                    photo: "https://image.tmdb.org/t/p/w1280" + data.results[randomNumber].backdrop_path
+                });
+            }   
+        )
+    }
+
+    render() {
+        return (
+            <div id="header_wrapper" style={{backgroundImage: `url(${this.state.photo})`}}>
+                <div id="text_wrapper">
+                    <p id="title">{this.state.movieTitle}</p>
+                    <p id="description">{this.state.movieDescription}</p>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default Header
