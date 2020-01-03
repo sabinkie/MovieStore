6692959571efc784b12e2ffe4832add355a8fc52
Low-class-dev <laper91@gmail.com>
2020-01-03 20:17:29 +0100

    added search by title with api request

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import axios from 'axios';
-class Navbar extends React.Component {
 
-    onClickNav = () => {
-        const movieId = 5;
-        axios.get(`https://api.themoviedb.org/3/movie/${movieId}?api_key=ad840cde5d9cba2a015a161594f2e2ae&language=en-US`,
-            { Authentication: 'api_key=ad840cde5d9cba2a015a161594f2e2ae' },
-            { Parameters: `movieId=${movieId}` })
+class Navbar extends React.Component {
+    state = { term: '' }
+
+    onInputChange = () => {
+        axios.get(`https://api.themoviedb.org/3/search/movie?api_key=ad840cde5d9cba2a015a161594f2e2ae&query=${this.state.term}`,
+            { Authentication: 'api_key=ad840cde5d9cba2a015a161594f2e2ae' })
     }
+
+    // onClickNav = () => {
+    //     axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=ad840cde5d9cba2a015a161594f2e2ae&language=en-US`,
+    //         { Authentication: 'api_key=ad840cde5d9cba2a015a161594f2e2ae' })
+    // }
+
 
     render() {
         return (
@@ -19,13 +25,17 @@ class Navbar extends React.Component {
                     <li className='navItem'><a href='#'>What's new</a></li>
                     <li className='navItem'><a onClick={this.onClickNav} href='#'>Movies</a></li>
                     <li className='navItem'><a onClick={this.onClickNav} href='#'>TV Series</a></li>
-                    <li className='navItem'><a href='#'>About</a></li>
+                    <li className='navItem'><a onClick={this.onInputChange} href='#'>Search by title: </a>
+                        <input type='text' value={this.state.term} onChange={(e) => {
+                            this.setState({ term: e.target.value })
+                        }}>
+                        </input>
+                    </li>
 
                 </ul>
             </nav>
         )
     }
 }
-
 
 export default Navbar; 
