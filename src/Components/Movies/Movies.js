9566c1206bef81b68bea3d4c33fbc54f824dc6d2
Low-class-dev <laper91@gmail.com>
2020-01-04 22:24:04 +0100

    added dumb categories for Movies and TV Series Components

--- src/Components/Movies/Movies.js
+++ src/Components/Movies/Movies.js
@@ -2,10 +2,41 @@ import React from 'react';
 import axios from 'axios';
 
 class Movies extends React.Component {
-    onClickNav = () => {
-        axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=ad840cde5d9cba2a015a161594f2e2ae&language=en-US`,
-            { Authentication: 'api_key=ad840cde5d9cba2a015a161594f2e2ae' })
+
+
+    render() {
+        return (
+            <section>
+                <div content="28">Action</div>
+                <div content="12">Adventure</div>
+                <div content="16">Animation</div>
+                <div content="35">Comedy</div>
+                <div content="80">Crime</div>
+                <div content="99">Documentary</div>
+                <div content="18">Drama</div>
+                <div content="10751">Family</div>
+                <div content="14">Fantasy</div>
+            </section>
+            <section>
+                <div content="36">History</div>
+                <div content="27">Horror</div>
+                <div content="10402">Music</div>
+                <div content="9648">Mystery</div>
+                <div content="10749">Romance</div>
+                <div content="878">Science Fiction</div>
+                <div content="10770">Thriller</div>
+                <div content="10752">War</div>
+                <div content="37">Western</div>
+            </section>
+        )
+
     }
+
+
+    // onClickNav = () => {
+    //     axios.get(`https://api.themoviedb.org/3/genre/movie/list?api_key=ad840cde5d9cba2a015a161594f2e2ae&language=en-US`,
+    //         { Authentication: 'api_key=ad840cde5d9cba2a015a161594f2e2ae' })
+    // }
 
 }
 
--- src/Components/TVseries/TVseries.js
+++ src/Components/TVseries/TVseries.js
@@ -3,9 +3,28 @@ import React from 'react';
 
 
 class TVseries extends React.Component {
-    render() {
+    render(props) {
         return (
-            <div>/</div>
+            <section>
+                <div content="10759">Action & Adventure</div>
+                <div content="16">Animation</div>
+                <div content="35">Comedy</div>
+                <div content="80">Crime</div>
+                <div content="99">Documentary</div>
+                <div content="18">Drama</div>
+                <div content="10751">Family</div>
+                <div content="10762">Kids</div>
+            </section>
+            <section>
+                <div content="9648">Mystery</div>
+                <div content="10763">News</div>
+                <div content="10764">Reality</div>
+                <div content="10765">Sci-Fi & Fantasy</div>
+                <div content="10766">Soap</div>
+                <div content="10767">Talk</div>
+                <div content="10768">War & Politics</div>
+                <div content="37">Western</div>
+            </section>
         )
     }
 }
